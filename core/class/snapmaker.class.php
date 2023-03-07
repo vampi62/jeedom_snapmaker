fb58edddc0f00fb39de522269a076ddfedea935e
vampi62 <104321401+vampi62@users.noreply.github.com>
2023-03-07 11:08:45 +0100

    init element

--- core/class/snapmaker.class.php
+++ core/class/snapmaker.class.php
@@ -94,6 +94,62 @@ class snapmaker extends eqLogic {
 
   // Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement
   public function postSave() {
+    $this->create_element('refresh','Rafraichir','action','other');
+    $this->create_element('pause'  ,'pause'     ,'action','other');
+    $this->create_element('start'  ,'start'     ,'action','other');
+    $this->create_element('stop'   ,'stop'      ,'action','other');
+    $this->create_element('resume' ,'resume'    ,'action','other');
+
+    $this->create_element('sendfile'  ,'sendfile'     ,'action','other');
+    $this->create_element('sendgcode' ,'sendgcode'    ,'action','other');
+
+    $this->create_element('reload','reload','action','other');
+    $this->create_element('unload','unload','action','other');
+
+    $this->create_element('setpauseifopen'  ,'setpauseifopen'  ,'action','other');
+    $this->create_element('unsetpauseifopen','unsetpauseifopen','action','other');
+    
+    $this->create_element('newtempnozzle','newtempnozzle','action','texte');
+    $this->create_element('newtempbed'   ,'newtempbed'   ,'action','texte');
+    $this->create_element('newspeed'     ,'newspeed'     ,'action','texte');
+    $this->create_element('newlazer'     ,'newlazer'     ,'action','texte');
+    $this->create_element('newzoffset'   ,'newzoffset'   ,'action','other');
+    $this->create_element('settempnozzle','settempnozzle','action','other');
+    $this->create_element('settempbed'   ,'settempbed'   ,'action','other');
+    $this->create_element('setspeed'     ,'setspeed'     ,'action','other');
+    $this->create_element('setlazer'     ,'setlazer'     ,'action','other');
+    $this->create_element('setzoffset'   ,'setzoffset'   ,'action','other');
+    $this->create_element('setlight'     ,'setlight'     ,'action','other');
+    $this->create_element('setfan'       ,'setfan'       ,'action','other');
+    $this->create_element('unsetlight'   ,'unsetlight'   ,'action','other');
+    $this->create_element('unsetfan'     ,'unsetfan'     ,'action','other');
+
+    $this->create_element('status'                    ,'status'                    ,'info','string');
+    $this->create_element('homed'                     ,'homed'                     ,'info','string');
+    $this->create_element('toolHead'                  ,'toolHead'                  ,'info','string');
+    $this->create_element('nozzleTemperature'         ,'nozzleTemperature'         ,'info','string');
+    $this->create_element('nozzleTargetTemperature'   ,'nozzleTargetTemperature'   ,'info','string');
+    $this->create_element('heatedBedTemperature'      ,'heatedBedTemperature'      ,'info','string');
+    $this->create_element('heatedBedTargetTemperature','heatedBedTargetTemperature','info','string');
+    $this->create_element('isFilamentOut'             ,'isFilamentOut'             ,'info','string');
+    $this->create_element('workSpeed'                 ,'workSpeed'                 ,'info','string');
+    $this->create_element('printStatus'               ,'printStatus'               ,'info','string');
+    $this->create_element('fileName'                  ,'fileName'                  ,'info','string');
+    $this->create_element('totalLines'                ,'totalLines'                ,'info','string');
+    $this->create_element('estimatedTime'             ,'estimatedTime'             ,'info','string');
+    $this->create_element('currentLine'               ,'currentLine'               ,'info','string');
+    $this->create_element('progress'                  ,'progress'                  ,'info','string');
+    $this->create_element('elapsedTime'               ,'elapsedTime'               ,'info','string');
+    $this->create_element('remainingTime'             ,'remainingTime'             ,'info','string');
+    $this->create_element('enclosure'                 ,'enclosure'                 ,'info','string');
+    $this->create_element('rotaryModule'              ,'rotaryModule'              ,'info','string');
+    $this->create_element('emergencyStopButton'       ,'emergencyStopButton'       ,'info','string');
+    $this->create_element('airPurifier'               ,'airPurifier'               ,'info','string');
+    $this->create_element('isEnclosureDoorOpen'       ,'isEnclosureDoorOpen'       ,'info','string');
+    $this->create_element('stopIfEnclosureDoorOpen'   ,'stopIfEnclosureDoorOpen'   ,'info','string');
+    $this->create_element('EnclosureLight'            ,'EnclosureLight'            ,'info','string');
+    $this->create_element('stopIfEnclosureFan'        ,'stopIfEnclosureFan'        ,'info','string');
+    $this->create_element('zoffset'                   ,'zoffset'                   ,'info','string');
   }
 
   // Fonction exécutée automatiquement avant la suppression de l'équipement
@@ -104,6 +160,18 @@ class snapmaker extends eqLogic {
   public function postRemove() {
   }
 
+  private function create_element($newcmd,$newname,$newtype,$newsubtype){
+    $newelement = $this->getCmd(null, $newcmd);
+    if (!is_object($newelement)) {
+      $newelement = new snapmakerCmd();
+      $newelement->setName(__($newname, __FILE__));
+    }
+    $newelement->setEqLogic_id($this->getId());
+    $newelement->setLogicalId($newcmd);
+    $newelement->setType($newtype);
+    $newelement->setSubType($newsubtype);
+    $newelement->save();
+  }
   /*
   * Permet de crypter/décrypter automatiquement des champs de configuration des équipements
   * Exemple avec le champ "Mot de passe" (password)
@@ -162,6 +230,100 @@ class snapmakerCmd extends cmd {
 
   // Exécution d'une commande
   public function execute($_options = array()) {
+    $eqlogic = $this->getEqLogic(); //récupère l'éqlogic de la commande $this
+    switch ($this->getLogicalId()) { //vérifie le logicalid de la commande
+      case 'refresh':
+        $eqlogic->checkAndUpdateCmd('status', $info['status']);
+        $eqlogic->checkAndUpdateCmd('homed', $info['homed']);
+        $eqlogic->checkAndUpdateCmd('toolHead', $info['toolHead']);
+        $eqlogic->checkAndUpdateCmd('nozzleTemperature', $info['nozzleTemperature']);
+        $eqlogic->checkAndUpdateCmd('nozzleTargetTemperature', $info['nozzleTargetTemperature']);
+        $eqlogic->checkAndUpdateCmd('heatedBedTemperature', $info['heatedBedTemperature']);
+        $eqlogic->checkAndUpdateCmd('heatedBedTargetTemperature', $info['heatedBedTargetTemperature']);
+        $eqlogic->checkAndUpdateCmd('isFilamentOut', $info['isFilamentOut']);
+        $eqlogic->checkAndUpdateCmd('workSpeed', $info['workSpeed']);
+        $eqlogic->checkAndUpdateCmd('printStatus', $info['printStatus']);
+        $eqlogic->checkAndUpdateCmd('fileName', $info['fileName']);
+        $eqlogic->checkAndUpdateCmd('totalLines', $info['totalLines']);
+        $eqlogic->checkAndUpdateCmd('estimatedTime', $info['estimatedTime']);
+        $eqlogic->checkAndUpdateCmd('currentLine', $info['currentLine']);
+        $eqlogic->checkAndUpdateCmd('progress', $info['progress']);
+        $eqlogic->checkAndUpdateCmd('elapsedTime', $info['elapsedTime']);
+        $eqlogic->checkAndUpdateCmd('remainingTime', $info['remainingTime']);
+        $eqlogic->checkAndUpdateCmd('toolHead', $info['toolHead']);
+        $eqlogic->checkAndUpdateCmd('enclosure', $info['moduleList']['enclosure']);
+        $eqlogic->checkAndUpdateCmd('rotaryModule', $info['moduleList']['rotaryModule']);
+        $eqlogic->checkAndUpdateCmd('emergencyStopButton', $info['moduleList']['emergencyStopButton']);
+        $eqlogic->checkAndUpdateCmd('airPurifier', $info['moduleList']['airPurifier']);
+        $eqlogic->checkAndUpdateCmd('isEnclosureDoorOpen', $info['isEnclosureDoorOpen']);
+      case 'enclosure':
+        $eqlogic->checkAndUpdateCmd('stopIfEnclosureDoorOpen', $info['stopIfEnclosureDoorOpen']);
+        $eqlogic->checkAndUpdateCmd('EnclosureLight', $info['EnclosureLight']);
+        $eqlogic->checkAndUpdateCmd('stopIfEnclosureDoorOpen', $info['stopIfEnclosureDoorOpen']);
+        break;
+      case 'file':
+        $eqlogic->checkAndUpdateCmd('stopIfEnclosureDoorOpen', $info['stopIfEnclosureDoorOpen']);
+        $eqlogic->checkAndUpdateCmd('EnclosureLight', $info['EnclosureLight']);
+        $eqlogic->checkAndUpdateCmd('stopIfEnclosureFan', $info['stopIfEnclosureFan']);
+        break;
+      case 'connect':
+        
+      break;
+      case 'disconnect':
+        
+      break;
+      case 'sendfile':
+        //uploadFile
+      break;
+      case 'startprintfile':
+        //uploadGcodeFile
+      break;
+      case 'settempnozzle':
+        
+      break;
+      case 'settempbed':
+        
+      break;
+      case 'setspeed':
+        
+      break;
+      case 'setlazer':
+        
+      break;
+      case 'pause':
+        
+      break;
+      case 'start':
+        
+      break;
+      case 'stop':
+        
+      break;
+      case 'resume':
+        
+      break;
+      case 'reload':
+        
+      break;
+      case 'unload':
+        
+      break;
+      case 'setpauseifopen':
+        
+      break;
+      case 'unsetpauseifopen':
+        
+      break;
+      case 'setzoffset':
+        
+      break;
+      case 'setlight':
+        
+      break;
+      case 'setfan':
+        
+      break;
+    }   
   }
 
   /*     * **********************Getteur Setteur*************************** */
--- desktop/php/snapmaker.php
+++ desktop/php/snapmaker.php
@@ -128,38 +128,31 @@ $eqLogics = eqLogic::byType($plugin->getId());
 
 							<legend><i class="fas fa-cogs"></i> {{Paramètres spécifiques}}</legend>
 							<div class="form-group">
-								<label class="col-sm-4 control-label">{{Nom du paramètre n°1}}
-									<sup><i class="fas fa-question-circle tooltips" title="{{Renseignez le paramètre n°1 de l'équipement}}"></i></sup>
+								<label class="col-sm-4 control-label">{{ip snapmaker}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Renseignez l'ip de l'imprimante snapmaker}}"></i></sup>
 								</label>
 								<div class="col-sm-6">
-									<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="param1" placeholder="{{Paramètre n°1}}">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="adresse">
 								</div>
 							</div>
 							<div class="form-group">
-								<label class="col-sm-4 control-label"> {{Mot de passe}}
-									<sup><i class="fas fa-question-circle tooltips" title="{{Renseignez le mot de passe}}"></i></sup>
+								<label class="col-sm-4 control-label">{{token}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{token utiliser pour se connecter a la snapmaker}}"></i></sup>
 								</label>
 								<div class="col-sm-6">
-									<input type="text" class="eqLogicAttr form-control inputPassword" data-l1key="configuration" data-l2key="password">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="token" disabled>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-sm-4 control-label">{{auto connect}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{activer la connexion automatique à chaque démarrage de l'imprimante}}"></i></sup>
+								</label>
+								<div class="col-sm-6">
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="auto">{{auto connect}}</label>
 								</div>
 							</div>
 							<!-- Exemple de champ de saisie du cron d'auto-actualisation avec assistant -->
 							<!-- La fonction cron de la classe du plugin doit contenir le code prévu pour que ce champ soit fonctionnel -->
-							<div class="form-group">
-								<label class="col-sm-4 control-label">{{Auto-actualisation}}
-									<sup><i class="fas fa-question-circle tooltips" title="{{Fréquence de rafraîchissement des commandes infos de l'équipement}}"></i></sup>
-								</label>
-								<div class="col-sm-6">
-									<div class="input-group">
-										<input type="text" class="eqLogicAttr form-control roundedLeft" data-l1key="configuration" data-l2key="autorefresh" placeholder="{{Cliquer sur ? pour afficher l'assistant cron}}">
-										<span class="input-group-btn">
-											<a class="btn btn-default cursor jeeHelper roundedRight" data-helper="cron" title="Assistant cron">
-												<i class="fas fa-question-circle"></i>
-											</a>
-										</span>
-									</div>
-								</div>
-							</div>
 						</div>
 
 						<!-- Partie droite de l'onglet "Équipement" -->
