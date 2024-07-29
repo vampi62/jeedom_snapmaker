e228c44b4f01e95649f25641ab38c9e244c7b29f
vampi62 <104321401+vampi62@users.noreply.github.com>
2024-07-29 21:00:47 +0200

    remove jquery

--- desktop/js/snapmaker.js
+++ desktop/js/snapmaker.js
@@ -14,16 +14,6 @@
 * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* Permet la réorganisation des commandes dans l'équipement */
-$("#table_cmd").sortable({
-  axis: "y",
-  cursor: "move",
-  items: ".cmd",
-  placeholder: "ui-state-highlight",
-  tolerance: "intersect",
-  forcePlaceholderSize: true
-})
-
 /* Fonction permettant l'affichage des commandes dans l'équipement */
 function addCmdToTable(_cmd) {
   if (!isset(_cmd)) {
@@ -70,38 +60,18 @@ function addCmdToTable(_cmd) {
   }
   tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove" title="{{Supprimer la commande}}"></i></td>'
   tr += '</tr>'
-  $('#table_cmd tbody').append(tr)
-  var tr = $('#table_cmd tbody tr').last()
+  document.getElementById('table_cmd').getElementsByTagName('tbody')[0].insertAdjacentHTML('beforeend', tr)
+  tr = document.querySelectorAll('#table_cmd tbody tr').last()
   jeedom.eqLogic.buildSelectCmd({
-    id:  $('.eqLogicAttr[data-l1key=id]').value(),
+    id:  document.querySelector('.eqLogicAttr[data-l1key=id]').value,
     filter: {type: 'info'},
     error: function (error) {
-      $('#div_alert').showAlert({message: error.message, level: 'danger'})
+      jeedomUtils.showAlert({message: error.message, level: 'danger'})
     },
     success: function (result) {
-      tr.find('.cmdAttr[data-l1key=value]').append(result)
-      tr.setValues(_cmd, '.cmdAttr')
+      tr.querySelector('.cmdAttr[data-l1key=value]').insertAdjacentHTML('beforeend', result)
+      tr.setJeeValues(_cmd, '.cmdAttr')
       jeedom.cmd.changeType(tr, init(_cmd.subType))
     }
-  })
-  $("#eqlogictab").off('click', '.listCmdInfo').on('click', '.listCmdInfo', function() {
-    var el = $(this).closest('.form-group').find('.eqLogicAttr')
-    jeedom.cmd.getSelectModal({ cmd: { type: 'info' } }, function(result) {
-      if (el.attr('data-concat') == 1) {
-        el.atCaret('insert', result.human)
-      } else {
-        el.value(result.human)
-      }
-    })
-  })
-  $("#eqlogictab").off('click', '.listCmdAction').on('click', '.listCmdAction', function() {
-    var el = $(this).closest('.form-group').find('.eqLogicAttr')
-    jeedom.cmd.getSelectModal({ cmd: { type: 'action' } }, function(result) {
-      if (el.attr('data-concat') == 1) {
-        el.atCaret('insert', result.human)
-      } else {
-        el.value(result.human)
-      }
-    })
   })
 }
